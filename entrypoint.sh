a326108cc29c3e6993a1c725f82b057c17378337
tamasfe <me@tamasfe.dev>
2022-06-12 23:36:22 +0200

    feat: user name, email and pat support

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,15 @@ git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 
 cd "${GITHUB_WORKSPACE}" || exit
 
-GIT_USER_NAME="${GITHUB_ACTOR}"
-GIT_USER_EMAIL="${GITHUB_ACTOR}@users.noreply.github.com"
+GIT_USER_NAME=${INPUT_NAME:-${GITHUB_ACTOR}}
+GIT_USER_EMAIL=${INPUT_EMAIL:-"${GIT_USER_NAME}@users.noreply.github.com"}
 
 git config user.name "${GIT_USER_NAME}"
 git config user.email "${GIT_USER_EMAIL}"
+
+if [ -n "${INPUT_TOKEN}" ]; then
+    git remote set-url origin "https://${GITHUB_ACTOR}:${INPUT_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+fi
 
 eval "auto-tag --commit $GITHUB_SHA --git-user-name $GIT_USER_NAME --git-user-email $GIT_USER_EMAIL $INPUT_PATHS"
 
